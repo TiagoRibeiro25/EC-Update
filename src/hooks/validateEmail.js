ebc8fb466aee955bf7feb78d18166331e0ecd694
Tiago Ribeiro <tiago.d.ribeiro@hotmail.com>
2023-02-13 18:15:14 +0000

    added comment to validate emails function

--- src/hooks/validateEmail.js
+++ src/hooks/validateEmail.js
@@ -1,3 +1,8 @@
+/**
+ * Function used to validate an email address.
+ * @param email - The email address to validate.
+ * @returns A boolean value.
+ */
 export function validateEmail(email) {
 	const emailRegex =
 		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}\])|(([a-zA-Z\-\d]+\.)+[a-zA-Z]{2,}))$/;

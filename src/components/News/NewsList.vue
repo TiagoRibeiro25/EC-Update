4f716158593ddd43dfcd3f916f0da98a04ce689b
Tiago Ribeiro <tiago.d.ribeiro@hotmail.com>
2023-02-22 17:52:52 +0000

    added message when there's no news

--- src/components/News/NewsList.vue
+++ src/components/News/NewsList.vue
@@ -19,9 +19,35 @@ watchEffect(() => {
 </script>
 
 <template>
-	<div>
+	<div v-if="news.length > 0">
 		<div v-for="newItem in delayedNews" :key="newItem.id">
 			<New :theme="theme" :newItem="newItem" />
 		</div>
 	</div>
+	<div v-else class="row" style="height: 190px">
+		<div class="col-12 d-flex justify-content-center align-items-center">
+			<h2
+				class="error-title mt-5 pt-5 text-center"
+				:class="theme ? 'error-title-dark' : 'error-title-light'"
+			>
+				Nenhuma notícia encontrada :(
+			</h2>
+		</div>
+	</div>
 </template>
+
+<style lang="scss" scoped>
+.error-title {
+	font-family: "Panton", sans-serif;
+	font-size: 2rem;
+	font-weight: 600;
+}
+
+.error-title-light {
+	color: #333333;
+}
+
+.error-title-dark {
+	color: #f8f9fa;
+}
+</style>

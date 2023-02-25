0421eea83cb01bc6bd3c12c5a6957ab390dce467
Tiago Ribeiro <tiago.d.ribeiro@hotmail.com>
2023-02-25 17:17:12 +0000

    finished fast actions in manage view

---
+++ src/components/Manage/FastActions.vue
@@ -0,0 +1,78 @@
+<script setup>
+import CreateNewBtn from "@/components/CreateNewBtn.vue";
+import { useUsersStore } from "@/stores/users";
+
+const props = defineProps({ theme: { type: Boolean, required: true } });
+
+const theme = props.theme;
+const userLogged = useUsersStore().getUserLogged();
+const isUserAdmin = userLogged.role === "admin";
+</script>
+
+<template>
+	<div
+		class="row mx-5 mt-5 d-flex flex-xl-row flex-column justify-content-xl-start justify-content-center"
+	>
+		<h2
+			class="fast-actions-title text-center"
+			:class="theme ? 'title-dark-theme' : 'title-light-theme'"
+		>
+			AÇÕES RÁPIDAS
+		</h2>
+		<div class="row mx-auto w-75 pl-xl-5">
+			<div
+				class="col-xl-4 d-flex justify-content-xl-start justify-content-center mt-xl-0 mt-3"
+			>
+				<CreateNewBtn
+					:theme="theme"
+					text="Adicionar Atividade"
+					type="ActivitiesCreate"
+				/>
+			</div>
+			<div
+				class="col-xl-4 d-flex justify-content-xl-start justify-content-center mt-xl-0 mt-3"
+			>
+				<CreateNewBtn
+					:theme="theme"
+					text="Adicionar Notícia"
+					type="NewsCreate"
+					:disable="!isUserAdmin"
+				/>
+			</div>
+			<div
+				class="col-xl-4 d-flex justify-content-xl-start justify-content-center mt-xl-0 mt-3"
+				:disable="!isUserAdmin"
+			>
+				<CreateNewBtn
+					:theme="theme"
+					text="Menu Admin"
+					type="AdminMenu"
+					:disable="!isUserAdmin"
+				/>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+$primary-color: #343e3d;
+$secondary-color: #aedcc0;
+$tertiary-color: #3fc380;
+$fourth-color: #ffffff;
+$fifth-color: #18516f;
+
+.fast-actions-title {
+	font-family: "Panton", sans-serif;
+	font-size: 1.5rem;
+	font-weight: 500;
+	margin-top: 1px;
+}
+
+.title-light-theme {
+	color: $primary-color;
+}
+
+.title-dark-theme {
+	color: $secondary-color;
+}
+</style>
--- src/pages/manage/ManageView.vue
+++ src/pages/manage/ManageView.vue
@@ -1,16 +1,39 @@
 <script setup>
 import Header from "@/components/Header.vue";
+import FastActions from "@/components/Manage/FastActions.vue";
 import { useUsersStore } from "@/stores/users";
+import { ref, onBeforeMount } from "vue";
 
 const theme = useUsersStore().isDarkMode();
+const loaded = ref(false);
+
+onBeforeMount(() => {
+	// check if the user logged has a role different from "unsigned"
+	const userLogged = useUsersStore().getUserLogged();
+
+	setTimeout(() => {
+		if (userLogged.role === "unsigned") $router.push({ name: "Home" });
+		else loaded.value = true;
+	}, 300);
+});
 </script>
 
 <template>
 	<div
 		class="wrapper px-xl-5 px-4 pt-3"
+		style="overflow-x: hidden"
 		:class="{ 'background-light': !theme, 'background-dark': theme }"
 	>
-		<Header title="GESTÃO" :theme="theme" />
+		<div v-if="loaded" class="content mx-auto">
+			<Header title="GESTÃO" :theme="theme" />
+			<FastActions :theme="theme" />
+		</div>
+		<div
+			v-else
+			class="loading-page pb-5 d-flex justify-content-center align-items-center flex-column"
+		>
+			<b-spinner variant="success" label="Carregando..."></b-spinner>
+		</div>
 	</div>
 </template>
 
@@ -18,11 +41,21 @@ const theme = useUsersStore().isDarkMode();
 $primary-color: #f8f9fa;
 $secondary-color: #333333;
 
+.wrapper,
+.loading-page {
+	min-height: 100vh;
+	min-height: 100dvh;
+}
+
 .background-light {
 	background-color: $primary-color;
 }
 
 .background-dark {
 	background-color: $secondary-color;
+}
+
+.content {
+	max-width: 1400px;
 }
 </style>

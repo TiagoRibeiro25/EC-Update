1807f0d923b85fddf41d369f903ad8bb689be40c
Tiago Ribeiro <tiago.d.ribeiro@hotmail.com>
2023-03-07 16:30:13 +0000

    replaced local quotes fetch with an external api

--- src/components/Home/Quote.vue
+++ src/components/Home/Quote.vue
@@ -1,24 +1,25 @@
 <script setup>
-import { fetchData } from "@/hooks/fetchData";
-import { ref, onBeforeMount } from "vue";
+import axios from "axios";
+import { onBeforeMount, ref } from "vue";
 
 const props = defineProps({ theme: { type: Boolean, required: true } });
 const theme = props.theme;
 
 const quote = ref({});
 
-// Get a random quote from the json file
+// Fetch a random quote from the API
 onBeforeMount(async () => {
 	try {
-		const quotes = await fetchData("quotes");
-		quote.value = quotes[Math.floor(Math.random() * quotes.length)]; // random quote
+		const res = await axios.get("https://quotes-api-fkca.onrender.com/api/quotes/random");
+		quote.value = res.data;
 	} catch (error) {
-		console.log(error);
 		quote.value = {
 			author: "Yan Arthus Bertrand",
 			quote: "A ecologia deve estar inscrita no ADN de tudo o que fazemos diariamente e ainda não entrou nos costumes",
 		};
-		console.log("There was an error fetching the quote. Using default quote instead.");
+		console.log(
+			`There was an error fetching the quote. Using default quote instead.\n Error: ${error}`
+		);
 	}
 });
 </script>
@@ -27,11 +28,11 @@ onBeforeMount(async () => {
 	<div
 		class="d-flex flex-column align-items-center justify-content-center mb-5 w-75 mx-auto"
 	>
-		<div v-if="!quote.content || !quote.author">
+		<div v-if="!quote.quote || !quote.author">
 			<b-spinner variant="success" label="Carregando..."></b-spinner>
 		</div>
 		<div v-else class="text-center" :class="!theme ? 'light-text' : 'dark-text'">
-			<p class="content mb-0">"{{ quote.content }}"</p>
+			<p class="content mb-0">"{{ quote.quote }}"</p>
 			<span class="author mt-0">{{ quote.author }}</span>
 		</div>
 	</div>
